8dc5652efdd7de9f4fabccbe9b89e6f2d9818458
Brayan Zavala <zavalabrayan244@gmail.com>
2020-05-18 00:01:59 -0500

    adds shows action and its tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class RecipesController < ApplicationController
-  def index 
+  def index
     @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 end
