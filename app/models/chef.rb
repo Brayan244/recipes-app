d6da63b1bfa6f5eec716faa96d4b054fba8e0b7e
Brayan Zavala <zavalabrayan244@gmail.com>
2020-05-17 19:31:12 -0500

    adds chef model validation with tests

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Chef < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :chef_name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  before_save { self.email = email.downcase }
 end
