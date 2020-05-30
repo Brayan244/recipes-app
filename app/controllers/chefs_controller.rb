190d123b7d5acf8472c4f5d7a4a1b45308b75517
Brayan Zavala <zavalabrayan244@gmail.com>
2020-05-30 18:37:50 -0500

    implements chefs signup and its tests

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
 class ChefsController < ApplicationController
-  def new; end
+  before_action :set_chef, only: [:show]
+
+  def new
+    @chef = Chef.new
+  end
+
+  def show; end
+
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = 'Chef was Sign up successfully!'
+      redirect_to chef_path(@chef)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_chef
+    @chef = Chef.find(params[:id])
+  end
+
+  def chef_params
+    params.require(:chef).permit(:chef_name, :email, :password, :confirm_password)
+  end
 end
--- test/integration/chef_sign_up_test.rb
+++ test/integration/chef_sign_up_test.rb
@@ -7,4 +7,39 @@ class ChefSignUpTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
   end
+
+  test 'reject invalid signup' do
+    get signup_path
+    assert_no_difference 'Chef.count' do
+      post chefs_path, params: {
+        chef: {
+          chef_name: ' ',
+          email: ' ',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+
+    assert_template 'chefs/new'
+    assert_select 'div.card-header'
+    assert_select 'div.card-body'
+  end
+
+  test 'accept valid signup' do
+    get signup_path
+    assert_difference 'Chef.count' do
+      post chefs_path, params: {
+        chef: {
+          chef_name: 'Brayan',
+          email: 'example@example.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+
+    follow_redirect!
+    assert_not flash.empty?
+  end
 end
